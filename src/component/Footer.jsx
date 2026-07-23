34d6bab635c28a0e64517d49d5af07a0d2f2796b
DipanshuGurjar123 <dipanshugurjar049@gmail.com>
2026-07-23 12:37:16 +0530

    change in footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
 
         <div className='max-w-6xl mx-auto p-5'>
 
-            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-between pt-10 h-auto pb-6 gap-20'>
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-between pt-10 h-auto pb-6 sm:gap-4 lg:gap-20'>
 
                 <div className='pb-22'>
                     <p className='font-bold underline'>Social Media -</p>
